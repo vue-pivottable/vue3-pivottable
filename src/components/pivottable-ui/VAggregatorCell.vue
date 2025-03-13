1c49e3171c29658a84f05dcc1ca64d0389b25290
hyemyn2 <hyemyn2@gmail.com>
2025-03-13 14:03:01 +0900

    feat: set aggregatorCell

--- src/components/pivottable-ui/VAggregatorCell.vue
+++ src/components/pivottable-ui/VAggregatorCell.vue
@@ -1,1 +1,68 @@
 <!-- aggregatorCell -->
+<template>
+    <template>
+        <td
+            v-if="this.$slots.aggregatorCell"
+            class="pvtVals pvtText"
+        >
+            <slot name="aggregatorCell"></slot>
+        </td>
+        <td
+            v-else
+            class="pvtVals"
+        >
+            <div>
+                <VDropdown
+                    :values="Object.keys(this.aggregatorItems)"
+                    :value="aggregatorName"
+                    @input="handleInputAggregatorName"
+                >
+                </VDropdown>
+                <a
+                    class="pvtRowOrder"
+                    role="button"
+                    @click="handleClickRowOrder"
+                >
+                </a>
+                <a
+                    class="pvtColOrder"
+                    role="button"
+                    @click="handleClickColOrder"
+                >
+
+                </a>
+            </div>
+            <template
+                v-if="numValsAllowed > 0"
+            >
+                <VDropdown
+                    v-for="(item, i) in numValsAllowed"
+                    :key="i"
+                    :values="Object.keys(this.attrValues).filter(e =>
+                    !this.hiddenAttributes.includes(e) &&
+                    !this.hiddenFromAggregators.includes(e))"
+                    :value="vals[i]"
+                    @input="(i) => {handleInputHidden(value, i)}"
+                >
+
+                </VDropdown>
+            </template>
+        </td>
+    </template>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import VDropdown from './VDropdown'
+import defaultProps from '../../helper/defaultProps'
+
+const aggregatorItems = computed(() => (defaultProps.aggregators) || aggregators)
+const numValsAllowed = computed(() => aggregatorItems[this.propsData.aggregatorName]([])().numInputs || 0)
+const handleInputAggregatorName = (value) => this.propUpdater('aggregatorName')(value)
+// const handleInputHidden = (value) => Object.keys(this.attrValues).filter(e => !this.hiddenAttributes.includes(e) && !this.hiddenFromAggregators.includes(e))
+const handleInputHidden = (value, i) => this.propsData.vals.splice(i, 1, value)
+const handleClickRowOrder = () => { this.propUpdater('rowOrder')(this.sortIcons[this.propsData.rowOrder].next) }
+const handleClickColOrder = () => { this.propUpdater('colOrder')(this.sortIcons[this.propsData.colOrder].next) }
+</script>
+
+// aggregators props로 넘겨주기
--- src/components/pivottable-ui/VRendererCell.vue
+++ src/components/pivottable-ui/VRendererCell.vue
@@ -1,1 +1,52 @@
 <!-- rendererCell -->
+<template>
+    <template>
+        <td
+            v-if="this.$slots.rendererCell"
+            class="pvtRenderers pvtVals pvtText"
+        >
+            <slot name="rendererCell"></slot>
+        </td>
+        <td
+            v-else
+            class="pvtRenderers"
+        >
+            <VDropdown
+                :values="Object.keys(rendererItems)"
+                :value="rendererName"
+                @input="emitPropUpdater($event)"
+            >
+            </VDropdown>
+        </td>
+    </template>
+    <td>
+
+    </td>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import TableRenderer from './TableRenderer'
+import VDropdown from './VDropdown'
+import defaultProps from '../../helper/defaultProps'
+
+const props = defineProps({
+  rendererName: {
+    type: String,
+    default: ''
+  }
+})
+const emitPropUpdater = (e) => {
+  // this.propUpdater('rendererName')(value)
+  // this.propUpdater('renderer')(rendererItems[props.rendererName])
+  this.$emit('propUpdater', { key: 'rendererName', value: e.target.value })
+  this.$emit('propUpdater', { key: 'renderer', value: rendererItems[props.rendererName] })
+}
+const rendererItems = computed(() => (defaultProps.renderers) || Object.assign({}, TableRenderer))
+</script>
+
+// props: rendererName
+// emit : propUpdater
+// TableRenderer 호출 파일 확인
+// defaultProps 경로 확인
+// slot 방식 확인
