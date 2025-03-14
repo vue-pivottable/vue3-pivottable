823ac40c4828692cdf23689a43fa17110a293e7f
hyemyn2 <hyemyn2@gmail.com>
2025-03-14 18:03:03 +0900

    feat: set propUpdater

--- src/components/pivottable-ui/VAggregatorCell.vue
+++ src/components/pivottable-ui/VAggregatorCell.vue
@@ -29,7 +29,7 @@
               role="button"
               @click="emitPropUpdater({ key: 'colOrder', value: sortIcons[props.colOrder].next})"
           >
-              {{ sortIcons[props.colOrder].colSymbol }}
+              {{ sortIconsOfColOrder }}
           </a>
       </div>
       <template v-if="numValsAllowed">
@@ -74,9 +74,11 @@ const props = defineProps({
       return ['key_a_to_z', 'value_a_to_z', 'value_z_to_a'].indexOf(value) !== -1
     }
   },
-  propsData: {
-    type: Object,
-    default: () => {}
+  vals: {
+    type: Array,
+    default: function () {
+      return []
+    }
   },
   attrValues: {
     type: Object,
@@ -98,10 +100,11 @@ const aggregatorItems = computed(() => (props.aggregators) || aggregators)
 const numValsAllowed = computed(() => aggregatorItems.value[props.aggregatorName]([])().numInputs || 0)
 const fetchValues = computed(() => Object.keys(props.attrValues).filter(e => !props.hiddenAttributes.includes(e) && !props.hiddenFromAggregators.includes(e)))
 
+const sortIconsOfColOrder = computed(() => {
+  console.log(sortIcons[props.colOrder].colSymbol)
+  return sortIcons[props.colOrder].colSymbol
+})
+
 const emitValSlice = (e, i) => { emit('update:valSlice', { key: i, value: e.target.value }) }
 const emitPropUpdater = (updateObj) => emit('update:propUpdater', updateObj)
-// const emitPropUpdaterRowOrder = () => emit('update:propUpdater', { key: 'rowOrder', value: sortIcons[props.rowOrder].next })
-// const emitPropUpdaterColOrder = () => emit('update:propUpdater', { key: 'colOrder', value: sortIcons[props.colOrder].next })
 </script>
-
-// @input="(e) => emitPropUpdater({ key: 'aggregatorName', value: e.target.value})"
--- src/composables/propData.js
+++ src/composables/propData.js
@@ -4,7 +4,9 @@ const newProps = ref({})
 
 export function usePropsData ($props) {
   newProps.value = $props
-  const propUpdater = (key, value) => (newProps[key] = value)
+  const propUpdater = ({ key, value }) => {
+    newProps[key] = value
+  }
   const setProps = ($props) => (newProps.value = $props)
   return {
     newProps,
