dfe236cac704d9bad7ea2886039566c5d7a4eb98
hyemyn2 <hyemyn2@gmail.com>
2025-04-14 11:31:47 +0900

    refactor: refactoring VFilterBox.vue

--- src/components/pivottable-ui/VFilterBox.vue
+++ src/components/pivottable-ui/VFilterBox.vue
@@ -25,14 +25,14 @@
         <a
           class="pvtButton"
           role="button"
-          @click="removeValuesFromFilter(filterBoxValues.filter(matchesFilter))"
+          @click="removeValuesFromFilter(filteredList)"
         >
           {{  localeStrings.selectAll }}
         </a>
         <a
           class="pvtButton"
           role="button"
-          @click="addValuesToFilter(filterBoxValues.filter(matchesFilter))"
+          @click="addValuesToFilter(filteredList)"
         >
           {{ localeStrings.selectNone }}
         </a>
@@ -42,7 +42,7 @@
       class="pvtCheckContainer"
     >
       <p
-        v-for="x in shown"
+        v-for="x in filteredList"
         :key="x"
         :class="{'selected': !(x in unselectedValues)}"
         @click="toggleValue(x)"
@@ -69,13 +69,9 @@
 
 <script setup>
 import { ref, computed, inject } from 'vue'
-/** inject */
 const sorters = inject('sorters')
 const menuLimit = inject('menuLimit')
 const localeStrings = inject('localeStrings')
-const allFilters = inject('allFilters')
-const filterBoxValues = computed(() => allFilters[props.filterBoxKey])
-/** props */
 const props = defineProps({
   unselectedFilterValues: {
     type: Object,
@@ -88,19 +84,21 @@ const props = defineProps({
   zIndex: {
     type: Number,
     default: 0
+  },
+  filterBoxValues: {
+    type: Array,
+    default: () => []
   }
 })
-/** state */
 const filterText = ref('')
-const showMenu = ref(filterBoxValues.value.length < menuLimit)
-const shown = computed(() => filterBoxValues.value.filter(matchesFilter).sort(sorters(props.filterBoxKey)))
+const showMenu = ref(props.filterBoxValues.length < menuLimit)
+const sortedList = [...props.filterBoxValues].sort(sorters(props.filterBoxKey))
+const filteredList = computed(() => sortedList.filter(matchesFilter))
 const unselectedValues = ref(props.unselectedFilterValues)
-/** emit */
 const emit = defineEmits([
   'update:zIndexOfFilterBox',
   'update:unselectedFilterValues'
 ])
-/** method */
 const moveFilterBoxToTop = (e) => {
   e.stopPropagation()
   emit('update:zIndexOfFilterBox', props.filterBoxKey)
@@ -114,7 +112,7 @@ const addValuesToFilter = (values) => {
     r[v] = true
     return r
   }, Object.assign({}, unselectedValues.value))
-  emit('update:unselectedFilterValues', { attribute: props.filterBoxKey, filterValues })
+  emit('update:unselectedFilterValues', { key: props.filterBoxKey, value: filterValues })
 }
 const removeValuesFromFilter = (values) => {
   const filterValues = values.reduce((r, v) => {
@@ -123,7 +121,7 @@ const removeValuesFromFilter = (values) => {
     }
     return r
   }, Object.assign({}, unselectedValues.value))
-  emit('update:unselectedFilterValues', { attribute: props.filterBoxKey, filterValues })
+  emit('update:unselectedFilterValues', { key: props.filterBoxKey, value: filterValues })
 }
 const toggleValue = (value) => {
   if (value in unselectedValues.value) {
@@ -134,13 +132,13 @@ const toggleValue = (value) => {
 }
 const selectOnly = (e, value) => {
   e.stopPropagation()
-  setValuesInFilter(props.filterBoxKey, filterBoxValues.value.filter(y => y !== value))
+  setValuesInFilter(props.filterBoxKey, props.filterBoxValues.filter(y => y !== value))
 }
-const setValuesInFilter = (attribute, values) => {
+const setValuesInFilter = (filterBoxKey, values) => {
   const filterValues = values.reduce((r, v) => {
     r[v] = true
     return r
   }, {})
-  emit('update:unselectedFilterValues', { attribute, filterValues })
+  emit('update:unselectedFilterValues', { key: filterBoxKey, value: filterValues })
 }
 </script>
