cb20a9a4f64625259d1f6da6570b646f62b29eb9
Seungwoo321 <seungwoo321@gmail.com>
2025-04-18 16:17:10 +0900

    chore: update demo code

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,44 @@
 <template>
   <div>
-    <VPivottableUi
-      :data="tips"
-      :rows="['Payer Smoker']"
-      :cols="['Payer Gender']"
+    <h1 style="text-align: center">Vue3 Pivottable</h1>
+    <CsvUploader
+      :initial-data="initialData"
+      :initial-filename="initialFilename"
+      @data-parsed="onDataParsed"
     >
-      <!-- <template v-slot:pvtAttr="props">
+      <template #default="{ data }">
+        <VPivottableUi
+          v-if="data.length > 0"
+          :data="data"
+          :rows="rows"
+          :cols="cols"
+          :vals="vals"
+          :aggregatorName="aggregatorName"
+          :rendererName="rendererName"
+          :sorters="sorters"
+        >
+          <!-- <template v-slot:pvtAttr="props">
           {{ props }}
         </template> -->
 
-      <!-- <template v-slot:outputSlot>
+          <!-- <template v-slot:outputSlot>
           <div>slot</div>
         </template> -->
-      <!-- <template v-slot:outputSlot="outputSlot">
+          <!-- <template v-slot:outputSlot="outputSlot">
           {{ outputSlot }}
         </template> -->
-    </VPivottableUi>
+        </VPivottableUi>
+      </template>
+    </CsvUploader>
   </div>
 </template>
 <script setup>
+import { ref } from 'vue'
 import tips from './tips.js'
+import CsvUploader from './CsvUploader.vue'
+import { PivotUtilities } from '@/'
+const initialData = ref(tips)
+const initialFilename = ref('샘플 데이터셋: Tips')
 // const tableOptions = {
 //   clickCallback: function (e, value, filters, pivotData) {
 //     const values = []
@@ -29,6 +48,29 @@ import tips from './tips.js'
 //     alert(values.join('\n'))
 //   }
 // }
+const rows = ref(['Payer Gender'])
+const cols = ref(['Day of Week'])
+const aggregatorName = ref('Sum')
+const sorters = ref({
+  'Day of Week': PivotUtilities.sortAs([
+    'Monday',
+    'Thursday',
+    'Wednesday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ])
+})
+const vals = ref(['Tip'])
+const rendererName = ref('Table Heatmap')
+const onDataParsed = (data) => {
+  rows.value = []
+  cols.value = []
+  vals.value = []
+  sorters.value = []
+  rendererName.value = 'Table'
+  aggregatorName.value = 'Count'
+}
 </script>
 
 <style scoped></style>
