72acfaa4f9ec0a08ed5b5628d2725f182fd8aa44
gingerbeerlime <lsumin1127@gmail.com>
2025-06-04 10:10:13 +0900

    refactor: convert redColorScaleGenerator.js to TypeScript #125

--- src/helper/redColorScaleGenerator.ts
+++ src/helper/redColorScaleGenerator.ts
@@ -1,7 +1,7 @@
-export function redColorScaleGenerator(values) {
+export function redColorScaleGenerator(values: number[]) {
   const min = Math.min.apply(Math, values)
   const max = Math.max.apply(Math, values)
-  return (x) => {
+  return (x: number) => {
     const nonRed = 255 - Math.round((255 * (x - min)) / (max - min))
     return { backgroundColor: `rgb(255,${nonRed},${nonRed})` }
   }
